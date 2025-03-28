c0ba4d280adeb28263981ea53aae4b80814f50f5
VyshnaviMulakalapalli <vyshnavi.mulakalapalli@unh.edu>
2025-03-28 13:13:00 -0400

    add serializer to handle user preference data

--- quote_requests/serializers.py
+++ quote_requests/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import QuoteRequest
+from .models import QuoteRequest, UserPreferences
 
 class QuoteRequestSerializer(serializers.ModelSerializer):
     business = serializers.CharField(source="business.username", read_only=True)
@@ -9,3 +9,9 @@ class QuoteRequestSerializer(serializers.ModelSerializer):
         model = QuoteRequest
         fields = ['business', 'expert', 'message', 'status', 'requested_at']
         read_only_fields = ['business', 'expert', 'requested_at']
+
+class UserPreferencesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserPreferences
+        fields = ['saved_location', 'saved_expertise', 'min_rating', 'last_searched_at']
+        read_only_fields = ['last_searched_at']
